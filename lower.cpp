2c56e9d257ae800cbdbfd8d947b96d13971dde59
Ellen Avrumova <ellenavrumova@Ellens-Air.home>
2021-09-26 13:19:53 -0400

    Improved formatting

--- lower.cpp
+++ lower.cpp
@@ -2,7 +2,6 @@
 
 std::string lower(int length) {
     std::cout << "Input side length: " << length << std::endl;
-    std::cout << "Shape: " << std::endl;
     std::string shape;
     for(int row = 1; row <= length; row++) {
         for(int col = 1; col <= row; col++) {
