38bbc7c96f13b9f4d7826002ec07eae8d8bda6bb
Ellen Avrumova <ellenavrumova@Ellens-Air.home>
2021-09-26 13:04:31 -0400

    Variable name modified

--- lower.cpp
+++ lower.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-std::string lower(int size) {
-    std::cout << "Input side length: " << size << std::endl;
+std::string lower(int length) {
+    std::cout << "Input side length: " << length << std::endl;
     std::cout << "Shape: " << std::endl;
     std::string shape;
-    for(int row = 1; row <= size; row++) {
+    for(int row = 1; row <= length; row++) {
         for(int col = 1; col <= row; col++) {
             shape += "*";
         }
