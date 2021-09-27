d2a39920fabd647b896b8d1174e5d5e4b3efa31d
Ellen Avrumova <ellenavrumova@Ellens-Air.home>
2021-09-26 21:44:33 -0400

    Fixed output

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -22,11 +22,9 @@ std::string trapezoid(int width, int height) {
     else {
         std::cout << "Shape: " << std::endl;
         std::cout << "trapezoid(" << width << "," << height << "): " << std::endl;
-        std::string row = std::string(width, '*');
-        shape += row += "\n";
         for(int col = 0; col < height; col++) {
             for(int row = 0; row < width; row++) {
-                if(row <= col || row >= width-col-1) {
+                if(row < col || row > width-col-1) {
                     shape += " ";
                 }
                 else {
